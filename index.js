0df92226c36a1386d3fdca8c34951eef10b17783
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-05-20 22:55:10 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+multiply;
+
+let random = Math.ceil(Math.random() * 10);
+random;
+
+
+let num3 = 12;
+let num4 = 3;
+let mod = num3 / num4;
+mod;
+
+let max = Math.max(1, 5, 6, 12, 20);
+max;
